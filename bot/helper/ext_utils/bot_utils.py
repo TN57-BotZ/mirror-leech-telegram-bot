8332d0ff3be3f28acf6dd2672388f5db99f6971f
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2022-01-18 19:02:16 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -127,7 +127,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
-            msg += f"<b>═══════ @KristyCloud ═══════\n\nName : {download.name()}</b>"
+            msg += f"<b>═══════ @TN57_BotZ ═══════\n\nName : {download.name()}</b>"
             msg += f"\n<b>Status : {download.status()}</b>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
